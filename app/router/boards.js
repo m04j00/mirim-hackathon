2cd16b89bb733139d816b83b1ac5286a113517ac
LEE MINJI <m04j00@gmail.com>
2021-09-25 21:56:28 +0900

    코드 정리

--- app/router/boards.js
+++ app/router/boards.js
@@ -11,12 +11,12 @@ router.use(express.json());
 router.post('/', function (req, res) {
     let title = req.body.title; //requst body 'title'로 넘어온 값을 title 변수에 저장
     let description = req.body.description; //requst body 'description'으로 넘어온 값을 description 변수에 저장
- 
+
     let sql = 'INSERT INTO board (title, description) VALUES(?, ?);';
     //sql insert 구문, title, description에 body로 넘어온 값을 ? 자리에 넣는다.
     let params = [title, description];
     //params로 insert에 넣을 값을 배열화한다.
-    
+
     //첫번째 인자 : sql 구문
     //두번째 인자 : ? 자리에 들어갈 파라미터 전달값 
     // function의 result : 반환값
@@ -26,14 +26,14 @@ router.post('/', function (req, res) {
             return res.sendStatus(400); // 400 반환
         }
         res.json(req.body); // 받은 값 반환
-        console.log("result : " + JSON.stringify(req.body)); 
+        console.log(result);
     });
 })
 
 // READ
-router.get('/', function(req,res){
+router.get('/', function (req, res) {
     let sql = 'select * from board;'; //sql 구문
-    
+
     //첫번째 인자 : sql 구문
     //function - result : sql문에 따른 반환 데이터
     connection.query(sql, function (err, result) {
@@ -47,7 +47,7 @@ router.get('/', function(req,res){
 })
 
 // UPDATE
-router.post('/:id', function(req, res){
+router.post('/:id', function (req, res) {
     let id = req.params.id; // 파라미터로 받음 id값, 게시글의 고유 번호이다.
     let title = req.body.title; // 변경된 타이틀을 body로 가져온다. 
     let description = req.body.description; // 변경된 내용물을 body로 가져온다. 
@@ -56,8 +56,8 @@ router.post('/:id', function(req, res){
     let params = [title, description, id]; // 위 구문 물음표 순서대로 값을 넣어준다. 
 
     connection.query(sql, params, function (err, result) {
-        if(err) console.log(err);
-        if(params[0].length == 0 || params[1].length == 0){
+        if (err) console.log(err);
+        if (params[0].length == 0 || params[1].length == 0) {
             return res.send('값을 다 채워주세요.');
         }
         res.json(req.body);
@@ -66,12 +66,12 @@ router.post('/:id', function(req, res){
 })
 
 // DELETE
-router.delete('/:id', function(req, res){
+router.delete('/:id', function (req, res) {
     let id = req.params.id; // 파라미터로 받음 id값, 게시글의 고유 번호이다.
 
     let sql = 'DELETE from board where id = ?'; // delete sql 구문
     connection.query(sql, id, function (err, result) {
-        if(err) console.log(err);
+        if (err) console.log(err);
         res.send('삭제되었습니다.');
         console.log(result);
     });
