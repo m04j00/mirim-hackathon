d150f84a9852c15319e21987b59008e0aca2a5ed
LEE MINJI <m04j00@gmail.com>
2021-09-25 19:18:06 +0900

    READ 구현

--- app/router/boards.js
+++ app/router/boards.js
@@ -8,15 +8,28 @@ router.use(express.urlencoded({
 router.use(express.json());
 
 router.post('/', function (req, res) {
-    let title = req.query.title;
-    let description = req.query.description;
+    let title = req.body.title;
+    let description = req.body.description;
     let params = [title, description];
-
-    let sql = 'INSERT into board (title, description) VALUES(?, ?);';
+    //console.log(params);
+    let sql = 'INSERT INTO board (title, description) VALUES(?, ?);';
 
     connection.query(sql, params, function (err, result) {
-        if (err) return res.sendStatus(400);
-
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        res.json(req.body);
+        console.log("result : " + JSON.stringify(req.body));
+    });
+})
+router.get('/', function(req,res){
+    let sql = 'select * from board;';
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
         res.json(result);
         console.log("result : " + JSON.stringify(result));
     });
