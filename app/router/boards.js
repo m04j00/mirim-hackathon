62ba6aa26b226e4cfcfdedf90623998aab9becf3
LEE MINJI <m04j00@gmail.com>
2021-09-25 20:57:02 +0900

    UPDATE 구현

--- app/router/boards.js
+++ app/router/boards.js
@@ -15,9 +15,8 @@ router.post('/', function (req, res) {
     let sql = 'INSERT INTO board (title, description) VALUES(?, ?);';
 
     connection.query(sql, params, function (err, result) {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(400);
+        if(params[0].length == 0 || params[1].length == 0){
+            return res.send('값을 다 채워주세요.');
         }
         res.json(req.body);
         console.log("result : " + JSON.stringify(req.body));
@@ -32,6 +31,23 @@ router.get('/', function(req,res){
         }
         res.json(result);
         console.log("result : " + JSON.stringify(result));
+    });
+})
+router.post('/:id', function(req, res){
+    let id = req.params.id; // 파라미터로 받음 id값, 게시글의 고유 번호이다.
+    let title = req.body.title; // 변경된 타이틀을 body로 가져온다. 
+    let description = req.body.description; // 변경된 내용물을 body로 가져온다. 
+
+    let sql = 'UPDATE board SET title = ?, description=? WHERE id = ?'; // update sql 구문
+    let params = [title, description, id]; // 위 구문 물음표 순서대로 값을 넣어준다. 
+
+    connection.query(sql, params, function (err, result) {
+        if(err) console.log(err);
+        if(params[0].length == 0 || params[1].length == 0){
+            return res.send('값을 다 채워주세요.');
+        }
+        res.json(req.body);
+        console.log(result);
     });
 })
 module.exports = router;
